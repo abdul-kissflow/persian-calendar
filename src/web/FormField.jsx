492f418a54a850f2a806db3d980c0a2af219fe25
abdul-kissflow <abdul@kissflow.com>
2025-05-22 18:45:14 +0530

    Updated picker

--- src/web/FormField.jsx
+++ src/web/FormField.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Calendar } from 'react-multi-date-picker'
+import DatePicker, { DateObject } from 'react-multi-date-picker'
 import persian from 'react-date-object/calendars/persian'
 import persian_fa from 'react-date-object/locales/persian_fa'
 
@@ -7,13 +7,13 @@ function FormField(props) {
     const { value, actions } = props
     const { updateValue } = actions
     return (
-        <Calendar
+        <DatePicker
             calendar={persian}
             locale={persian_fa}
-            value={value}
+            currentDate={new DateObject(value)}
             onChange={(dateObject) => {
-                console.log(dateObject.format())
-                updateValue(dateObject.format())
+                console.log(dateObject.format('YYYY/MM/DD'))
+                updateValue(dateObject.format('YYYY/MM/DD'))
             }}
         />
     )
