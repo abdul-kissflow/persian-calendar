278350f8ba9e95169cda26846884c8c03b410dca
Abdul <87855483+abdul-kissflow@users.noreply.github.com>
2025-05-22 18:24:16 +0530

    Add files via upload

---
+++ src/web/FormField.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Calendar } from 'react-multi-date-picker'
+import persian from 'react-date-object/calendars/persian'
+import persian_fa from 'react-date-object/locales/persian_fa'
+
+function FormField(props) {
+    const { value, actions } = props
+    const { updateValue } = actions
+    return (
+        <Calendar
+            calendar={persian}
+            locale={persian_fa}
+            value={value}
+            onChange={updateValue}
+        />
+    )
+}
+
+export default FormField
